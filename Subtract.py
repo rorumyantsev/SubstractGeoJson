bb546b7aedee0a6812efd60c1770eed49c67b926
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-04-21 23:42:52 -0400

    Update Subtract.py

--- Subtract.py
+++ Subtract.py
@@ -2,11 +2,11 @@ import json
 import streamlit as st
 import shapely
 
-Zone1 = st.text_area ("input coordinates that will be substracted from", z1)
+Zone1 = st.text_area ("input coordinates that will be substracted from", "")
 Zone1_json = json.loads(Zone1)
 Zone_to_substract_from = shapely.geometry.Polygon(Zone1_json['geometry']['coordinates'][0])
 
-Zone2 = st.text_area ("input coordinates that will be substracted", z2)
+Zone2 = st.text_area ("input coordinates that will be substracted", "")
 Zone2_json = json.loads(Zone2)
 Zone_to_substract = shapely.geometry.Polygon(Zone2_json['geometry']['coordinates'][0])
 

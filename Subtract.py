67ed6f491d95c74ecb3ca0ac776a6a08abf7c6ca
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-05-03 19:25:09 -0400

    Update Subtract.py

--- Subtract.py
+++ Subtract.py
@@ -3,21 +3,15 @@ import streamlit as st
 import shapely
 
 Zone1 = st.text_area ("input coordinates that will be substracted from", "")
-if Zone1 == "":
-   continue
-else:
+try:
   Zone1_json = json.loads(Zone1)
   Zone_to_substract_from = shapely.geometry.Polygon(Zone1_json['geometry']['coordinates'][0])
 
 Zone2 = st.text_area ("input coordinates that will be substracted", "")
-if Zone2 == "":
-  continue
-else:
+try:
   Zone2_json = json.loads(Zone2)
   Zone_to_substract = shapely.geometry.Polygon(Zone2_json['geometry']['coordinates'][0])
 
-if Zone1 == "" or Zone2 =="":
-  continue
-else:
+try:
   result = (Zone_to_substract_from.difference(Zone_to_substract))
   st.write(shapely.to_geojson(result, indent=2))

76c938c1dd4b41b9965e7f26e2ff79d3891bf5e9
rorumyantsev <126847670+rorumyantsev@users.noreply.github.com>
2023-06-08 22:40:57 -0400

    Update Subtract.py

--- Subtract.py
+++ Subtract.py
@@ -2,6 +2,7 @@ import json
 import streamlit as st
 import shapely
 
+option = st.selectbox("Substract or add?",("Substract","Add")
 Zone1 = st.text_area ("input coordinates that will be substracted from", "")
 try:
   Zone1_json = json.loads(Zone1)
@@ -17,7 +18,10 @@ except:
   Zone2_json = "null"
   Zone_to_substract = "null"
 try:
-  result = (Zone_to_substract_from.difference(Zone_to_substract))
-  st.write(shapely.to_geojson(result, indent=2))
+  if option == "substract":
+    result = (Zone_to_substract_from.difference(Zone_to_substract))
+  else:
+    result = (Zone_to_substract_from.union(Zone_to_substract))                    
+  st.code(shapely.to_geojson(result, indent=2), language='json')
 except:
   st.write("enter valid geojsons")
